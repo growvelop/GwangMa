765d0d8bae27885fdd2acd2f71a0205d8a705d89
growvelop <100521222+growvelop@users.noreply.github.com>
2026-01-04 03:13:41 +0900

    Add files via upload

---
+++ bj_1958.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int dp[101][101][101];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+
+    int len1 = s1.length();
+    int len2 = s2.length();
+    int len3 = s3.length();
+
+    for (int i = 1; i <= len1; i++) {
+        for (int j = 1; j <= len2; j++) {
+            for (int k = 1; k <= len3; k++) {
+                if (s1[i - 1] == s2[j - 1] && s2[j - 1] == s3[k - 1]) {
+                    dp[i][j][k] = dp[i - 1][j - 1][k - 1] + 1;
+                }
+                else {
+                    dp[i][j][k] = max({ dp[i - 1][j][k], dp[i][j - 1][k], dp[i][j][k - 1] });
+                }
+            }
+        }
+    }
+
+    cout << dp[len1][len2][len3];
+
+}
